f0be2790a9457e586c971dbfac829b4888ec64be
atb9210 <achraf.zarouali1@gmail.com>
2023-01-16 00:43:19 +0100

    Aggiornamento struttura app

---
+++ static/js/edit_open.js
@@ -0,0 +1,19 @@
+$('.edit-card').click(function(event) {
+    event.preventDefault();
+    var card_id = $(this).data('card-id');
+    console.log(card_id);
+    $.ajax({
+        url: '/edit/' + card_id,
+        type: 'GET',
+        success: function(data) {
+            // populate the form fields with the returned data
+            console.log(data);  // log the entire data object
+            console.log(data.name);  // log the data.name value
+            console.log(data.id);   // log the data.id value
+            $('#nameModal').val(data[1]);
+            $('#idModal').val(data[0]);
+            //show the modal
+            $('#editModal').modal('show');
+        }
+    });
+});
---
+++ static/js/edit_submit.js
@@ -0,0 +1,17 @@
+$('#updateForm').submit(function(event) {
+      event.preventDefault();
+      var card_id = $('#idModal').val();
+      var new_name = $('#nameModal').val();
+      $.ajax({
+          url: '/update',
+          type: 'POST',
+          data: {
+              id: card_id,
+              name: new_name
+          },
+          success: function(data) {
+              window.location.href = '/';
+              $('#editModal').modal('hide')
+          }
+      });
+    });
