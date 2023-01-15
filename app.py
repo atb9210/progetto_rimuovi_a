d5d55ea3a327e1c5e991a7af00605e451bbeeb46
atb9210 <achraf.zarouali1@gmail.com>
2023-01-15 02:03:07 +0100

    Added frontend and backend DELETE button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__, template_folder='templates')
@@ -7,6 +7,8 @@ app = Flask(__name__, template_folder='templates')
 def form():
     return render_template("form.html")
 
+
+#Ricevi input modifica parola e ritorna input
 @app.route("/submit", methods=["POST"])
 def submit():
     name = request.form["name"]
@@ -14,6 +16,20 @@ def submit():
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)""")
     c.execute("INSERT INTO users (name) VALUES (?)", (name,))
+    conn.commit()
+    #query to retrieve the data from the table
+    c.execute("SELECT * FROM users")
+    data = c.fetchall()
+    conn.close()
+    return render_template("form.html",data=data)
+
+
+#Aggiunto il DELETE button
+@app.route("/card/<int:card_id>/delete", methods=["GET"])
+def delete_card(card_id):
+    conn = sqlite3.connect("database/database.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM users WHERE id=?", (card_id,))
     conn.commit()
     #query to retrieve the data from the table
     c.execute("SELECT * FROM users")
